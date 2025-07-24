1005dfa74d678ca9aafc7e87c0cf850efb065e73
TarunSuddi <tarunsuddi471@gmail.com>
2025-07-24 11:05:28 +0530

    save

---
+++ Simple.java
@@ -0,0 +1,11 @@
+package Demo;
+
+public class Simple {
+    public static void main(String[] args)
+    {
+        int n=10;
+        if(n>0)
+        System.out.println("Number is positive");
+        
+    }
+}
