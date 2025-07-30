abd108e23fe46ddc85690531390c11ed327abfe6
Tarunsuddi <tarunsuddi471@gmail.com>
2025-07-30 10:40:19 +0530

    save

---
+++ Number.java
@@ -0,0 +1,13 @@
+public class Number {
+    public static void main(String[] args)
+    {
+        int num=0;
+        if(num>0)
+        System.out.println("positive");
+        else if(num<0)
+        System.out.println("Negative");
+        else
+        System.out.println("Zero");
+    }
+    
+}
