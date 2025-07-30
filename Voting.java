eb28d80a2cb518f44d49e5d9789fd73bb6fdccb9
Tarunsuddi <tarunsuddi471@gmail.com>
2025-07-30 10:18:18 +0530

    save

---
+++ Voting.java
@@ -0,0 +1,14 @@
+public class Voting {
+    public static void main(String[] args){
+        int age=20;
+        if (age>=18)
+        {
+            System.out.println("user is eligible to vote");
+        }
+        else
+        {
+            System.out.println("user is not eligible to vote");
+        }
+    }
+    
+}
